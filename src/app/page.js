941db28028d91a84d9c4a556a1dc1d8e68928999
PromXP <adm.promxp@gmail.com>
2025-05-11 00:57:31 +0530

    changed the password to text

--- src/app/page.js
+++ src/app/page.js
@@ -68,7 +68,7 @@ export default function Home() {
             <div>
               <label className="block text-gray-700 mb-1">New Password</label>
               <input
-                type="password"
+                type="text"
                 className="w-full text-black px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
